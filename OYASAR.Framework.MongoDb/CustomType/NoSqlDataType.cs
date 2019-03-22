984c600fa0c47981a744e48f91a28afa93f1e491
Onur Y <onur.yasar@umur.com.tr>
2019-03-22 23:38:33 +0300

    added oyasar.framework.mongodb project

---
+++ OYASAR.Framework.MongoDb/CustomType/NoSqlDataType.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using OYASAR.Framework.Core.CustomType;
+using OYASAR.Framework.Core.Interface;
+using OYASAR.Framework.MongoDb.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OYASAR.Framework.MongoDb.CustomType
+{
+	public class NoSqlDataType<T> : NoSqlDataType, INoSqlDataType<T> where T : class
+	{
+		public IEnumerable<T> ToFind()
+		{
+			var collection = (MongoCollection<BsonModel<T>>) Data;
+			
+			IEnumerable<BsonModel<T>> result = collection.FindAll();
+
+			IList<T> list = new List<T>();
+
+			var text = result.ToJson();
+
+		    result.ToList().ForEach(x => list.Add(x.Data));
+
+			return list;
+		}
+
+		public T ToFilter(int id)
+		{
+			var collection = (MongoCollection<BsonModel<T>>)Data;
+
+			var result = collection.FindOneById(id);
+
+			return result.Data;
+		}
+	}
+}
---
+++ OYASAR.Framework.MongoDb/Model/BsonModel.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace OYASAR.Framework.MongoDb.Model
+{
+	public class BsonModel<T> where T : class
+	{
+		[BsonId]
+		public int Id { get; set; }
+			
+		public T Data { get; set; }
+	}
+}
