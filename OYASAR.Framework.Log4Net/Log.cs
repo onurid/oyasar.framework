4e9cbed4d4afdf5e71625155762be0111ab10ecf
Onur Y <onur.yasar@umur.com.tr>
2019-03-24 10:27:17 +0300

    modified log4net

--- OYASAR.Framework.Log4Net/Log.cs
+++ OYASAR.Framework.Log4Net/Log.cs
@@ -10,35 +10,26 @@ namespace OYASAR.Framework.Log4Net
 {
     public class Log : Core.Interface.ILog
     {
-        private readonly ILog log;
-
-        private static Type _type;
-
-        private static readonly ILog _log =  LogManager.GetLogger(_type);
-
-        public Log()
-        {
-            log = LogManager.GetLogger(_type);
-        }
+        private static ILog _log = null;
 
         public void Write(Result result)
         {
             var json = JsonConvert.SerializeObject(result);
 
             if (result.Success)
-                log.Info(json);
+                _log.Info(json);
             else
-                log.Warn(json);
+                _log.Warn(json);
         }
 
         public void Write(Exception ex)
         {
-            log.Error(ex.Message);
+            _log.Error(ex.Message);
         }
 
         public void Write(string message)
         {
-            throw new NotImplementedException();
+            _log.Fatal(message);
         }
 
         public static void SetLog4NetConfiguraiton(Type type)
@@ -51,7 +42,7 @@ namespace OYASAR.Framework.Log4Net
 
             log4net.Config.XmlConfigurator.Configure(repo, log4netConfig["log4net"]);
 
-            _type = type;
+            _log = LogManager.GetLogger(type);
 
             _log.Info("Application - Main is invoked");
         }
