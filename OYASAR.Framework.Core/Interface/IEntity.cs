3d75560949c158d256a61f2bd9493bbf9254f000
M. Burak Muştu <burak.mustu@hotmail.com>
2019-09-20 01:39:04 +0300

    core project interfaces architectural request

--- OYASAR.Framework.Core/Interface/IEntity.cs
+++ OYASAR.Framework.Core/Interface/IEntity.cs
@@ -1,9 +1,11 @@
 ﻿namespace OYASAR.Framework.Core.Interface
 {
-    public interface IEntity<TId>
+    public interface IEntity<TId> : IEntity
     {
         TId Id { get; }
 
         void SetId(TId id);
     }
+
+    public interface IEntity { }
 }
