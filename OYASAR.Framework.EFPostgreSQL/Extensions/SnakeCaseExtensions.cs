4710344d303a1d84cc09b1739879a8738d7165f1
Onur Yasar <onurid@yandex.com>
2019-10-06 17:07:33 +0300

    postgresql add extensions

---
+++ OYASAR.Framework.EFPostgreSQL/Extensions/SnakeCaseExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OYASAR.Framework.EFProvider.NetCore.PostgreSQL.Extensions
+{
+    public static class SnakeCaseExtensions
+    {
+        public static string ToSnakeCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            var prefixingUnderscores = Regex.Match(input, "^_+");
+            return prefixingUnderscores + Regex.Replace(input, "([a-z0-9])([A-Z])", "$1_$2").ToLower();
+        }
+    }
+}
