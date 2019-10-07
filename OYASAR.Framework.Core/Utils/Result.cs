b12017e97c58b0c5ca5575433af4b6ebc1d53af8
Onur Yasar <onurid@yandex.com>
2019-10-07 23:50:14 +0300

    result struct development

--- OYASAR.Framework.Core/Utils/Result.cs
+++ OYASAR.Framework.Core/Utils/Result.cs
@@ -5,6 +5,11 @@
         public bool Success { get; set; }
         public object Data { get; set; }
 
+        public static Result Ok()
+        {
+            return new Result { Success = true };
+        }
+
         public static Result Ok(object data)
         {
             return new Result { Success = true, Data = data };
@@ -18,6 +23,11 @@
         public static Result Error(Error data)
         {
             return new Result { Success = false, Data = data };
+        }
+
+        public static Result Error(string message)
+        {
+            return new Result { Success = false, Data = new Error { Message = message } };
         }
     }
 
